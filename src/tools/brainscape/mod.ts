0751d30398ba0c55892abf2f82395e65ef5e3a4c
tony <ayakovlenko@users.noreply.github.com>
2024-07-28 14:45:35 +0300

    deno fmt

--- src/tools/brainscape/mod.ts
+++ src/tools/brainscape/mod.ts
@@ -2,23 +2,23 @@ import { CardYaml } from "../../card.ts";
 import { parseCsv } from "../../deps.ts";
 
 export type BrainscapeCard = {
-    Question: string;
-    Answer: string;
+  Question: string;
+  Answer: string;
 };
 
 export function parseBrainscapeCsv(csvContent: string): { cards: CardYaml[] } {
-    const parsedCsv = parseCsv(
-        csvContent,
-        {
-            header: true,
-            skipFirstRow: true,
-        },
-    ) as BrainscapeCard[];
+  const parsedCsv = parseCsv(
+    csvContent,
+    {
+      header: true,
+      skipFirstRow: true,
+    },
+  ) as BrainscapeCard[];
 
-    const cards: CardYaml[] = parsedCsv.map((row) => ({
-        f: row.Question,
-        b: row.Answer,
-    }));
+  const cards: CardYaml[] = parsedCsv.map((row) => ({
+    f: row.Question,
+    b: row.Answer,
+  }));
 
-    return { cards };
+  return { cards };
 }
