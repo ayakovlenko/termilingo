22414c72c20d8405cd5850cd185fcd9171ea5be9
tony <ayakovlenko@users.noreply.github.com>
2024-08-02 10:08:10 +0300

    copy VienDinhCom/supermemo

---
+++ src/supermemo/mod.ts
@@ -0,0 +1,8 @@
+/**
+ * A fork of SM2 implementation from https://github.com/VienDinhCom/supermemo
+ * with the intention of modifying it into SM2+ as described below:
+ * https://www.blueraja.com/blog/477/a-better-spaced-repetition-learning-algorithm-sm2
+ *
+ * @module
+ */
+export * from "./supermemo.ts";
