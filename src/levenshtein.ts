d8234c13540a95123a4a5459d155a8d439b1024f
tony <ayakovlenko@users.noreply.github.com>
2024-10-16 17:10:48 +0300

    replace npm:fastest-levenshtein with jsr:@std/text

--- src/levenshtein.ts
+++ src/levenshtein.ts
@@ -1,4 +1,4 @@
-import { distance } from "fastest-levenshtein";
+import { levenshteinDistance } from "@std/text";
 
 // The function returns the Levenshtein ratio for the given strings. The
 // ratio is computed as follows and takes the value between 0 and 1 including:
@@ -7,7 +7,7 @@ import { distance } from "fastest-levenshtein";
 //
 // 1.0 means a perfect score; 0.0 means completely off target
 function ratio(src: string, dst: string): number {
-  const d = distance(src, dst);
+  const d = levenshteinDistance(src, dst);
 
   return (src.length + dst.length - d) / (src.length + dst.length);
 }
